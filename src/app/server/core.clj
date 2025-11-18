ed8d1d69f456a0064f309084ff454a4166ab717b
murilinhoPs <murilo.pescatori@nubank.com.br>
2025-11-18 12:19:21 -0300

    Adjust server state

--- src/app/server/core.clj
+++ src/app/server/core.clj
@@ -2,11 +2,12 @@
   "Servidor Ring/Http-kit com SSR básico pronto para hidratação UIx."
   (:require
    [app.server.diplomat.http-server :refer [app-routes]]
-   [app.server.helpers.logging :refer [wrap-request-logging]]
-   [app.server.state :as state]
+   [app.server.helpers.logging :refer [wrap-request-logging]] 
    [org.httpkit.server :refer [run-server]]
    [ring.middleware.params :refer [wrap-params]]
    [ring.middleware.reload :refer [wrap-reload]]))
+
+(defonce server (atom nil))
 
 (def app
   (-> app-routes
@@ -18,11 +19,24 @@
   [& _args]
   (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
     (println (format "🚀 Servidor disponível em http://localhost:%s" port))
-    (reset! state/server (run-server app {:port port}))))
+    (reset! server (run-server app {:port port}))))
+
+;; ----------------------------------------------------------------------------- 
+;; Server interativo com REPL
+;; -----------------------------------------------------------------------------
+
+(defn stop-server []
+  (when @server
+    (@server :timeout 100)
+    (reset! server nil)))
+
+(defn reset-server []
+  (stop-server)
+  (-main))
 
 (comment
-  (state/reset-server)
-  (state/stop-server)
-  @state/server
+  (reset-server)
+  (stop-server)
+  @server
   (app {:request-method :get
         :uri "/api"}))
